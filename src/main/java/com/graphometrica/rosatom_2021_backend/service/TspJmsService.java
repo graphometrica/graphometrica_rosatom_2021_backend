dec6e0aca7b7f2251be37778fa118a45ba9828eb
Kit <pinigins55@gmail.com>
2021-10-23 21:23:23 +0300

    Fix jms listener

--- src/main/java/com/graphometrica/rosatom_2021_backend/service/TspJmsService.java
+++ src/main/java/com/graphometrica/rosatom_2021_backend/service/TspJmsService.java
@@ -2,18 +2,14 @@ package com.graphometrica.rosatom_2021_backend.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.graphometrica.rosatom_2021_backend.config.ActiveMqProperties;
-import com.graphometrica.rosatom_2021_backend.model.Edge;
 import com.graphometrica.rosatom_2021_backend.model.TspInput;
 import com.graphometrica.rosatom_2021_backend.model.TspResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.activemq.command.ActiveMQQueue;
-import org.apache.activemq.command.ActiveMQTextMessage;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Slf4j
 @Service
@@ -39,11 +35,10 @@ public class TspJmsService implements TspJmsServiceInterface {
     }
 
     @JmsListener(destination = "${active-mq.rosatom.tsp-output-queue}", containerFactory = "jmsQueueListenerContainerFactory")
-    public void onTspResult(ActiveMQTextMessage message) {
+    public void onTspResult(String message) {
         try {
-            String resultRaw = message.getText();
-            TspResult result = mapper.readValue(resultRaw, TspResult.class);
-            log.info("Received Queue Message: " + result.getSolutionType());
+            log.info("Received Queue Message: " + message);
+            TspResult result = mapper.readValue(message, TspResult.class);
             graphService.updateRouter(result);
         } catch (Exception e) {
             log.error("Received Queue Exception : " + e);
--- src/main/java/com/graphometrica/rosatom_2021_backend/service/TspJmsServiceInterface.java
+++ src/main/java/com/graphometrica/rosatom_2021_backend/service/TspJmsServiceInterface.java
@@ -1,10 +1,9 @@
 package com.graphometrica.rosatom_2021_backend.service;
 
 import com.graphometrica.rosatom_2021_backend.model.TspInput;
-import org.apache.activemq.command.ActiveMQTextMessage;
 
 public interface TspJmsServiceInterface {
 
     void sendMessageTspSolver(TspInput input);
-    void onTspResult(ActiveMQTextMessage message);
+    void onTspResult(String message);
 }
